8028cae55abbc8ada7fe17497a403e8693e8e982
Данила Лебедев <DVLebedev@1cbit.ru>
2023-03-02 17:22:50 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles/mainPageStyles.css">
+    <title>MainPage</title>
+</head>
+<header>
+    <a href="/">Main Page</a> |
+    <a href="pages/about.php">About us</a>
+
+</header>
+<body>
+    <?php
+
+    ?>
+</body>
+<footer>
+    <div class="main-test-style">
+        <h1>All rights reserved &copy; 2023</h1>
+    </div>
+</footer>
+</html>
---
+++ pages/about.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../styles/mainPageStyles.css">
+    <title>MainPage</title>
+</head>
+<header>
+    <a href="/">Main Page</a> |
+    <a href="about.php">About us</a>
+
+</header>
+<body class="new-style-obla">
+<?php ?>
+<div>
+    <h1>aboutUs</h1>
+    <img src="../resours/obla.png">
+</div>
+</body>
+<footer>
+    <div class="main-test-style">
+        <h1>All rights reserved &copy; 2023</h1>
+    </div>
+</footer>
+</html>
